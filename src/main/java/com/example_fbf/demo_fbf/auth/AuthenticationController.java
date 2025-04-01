324de929099143e4373f00920c6f8bb5f87bae00
HoangPhuc <phucnguyeho@gmail.com>
2025-04-01 15:08:23 +0700

    prepare for simple auth controller

---
+++ src/main/java/com/example_fbf/demo_fbf/auth/AuthenticationController.java
@@ -0,0 +1,22 @@
+package com.example_fbf.demo_fbf.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        //
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+        //
+    }
+
+}
---
+++ src/main/java/com/example_fbf/demo_fbf/auth/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.example_fbf.demo_fbf.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String address;
+}
