a04590f39789954fc2b9aa3910bac380fbac0d5b
HoangPhuc <phucnguyeho@gmail.com>
2025-04-01 12:18:25 +0700

    setup FbfUser, add role

---
+++ src/main/java/com/example_fbf/demo_fbf/entity/FbfRole.java
@@ -0,0 +1,6 @@
+package com.example_fbf.demo_fbf.entity;
+
+public enum FbfRole {
+    FBF_USER,
+    ADMIN
+}
--- src/main/java/com/example_fbf/demo_fbf/entity/FbfUser.java
+++ src/main/java/com/example_fbf/demo_fbf/entity/FbfUser.java
@@ -2,7 +2,11 @@ package com.example_fbf.demo_fbf.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -13,7 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 @ToString
 @Builder
-public class FbfUser {
+public class FbfUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,10 +29,42 @@ public class FbfUser {
     private String phoneNumber;
     private String address;
 
+    @Enumerated(EnumType.STRING)
+    private FbfRole fbfRole;
+
     @OneToOne(mappedBy = "user")
     private Cart cart;
 
     @OneToMany(mappedBy = "user")
     private List<FbfOrder> orders;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(fbfRole.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+//        return UserDetails.super.isAccountNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+//        return UserDetails.super.isAccountNonLocked();
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+//        return UserDetails.super.isCredentialsNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+//        return UserDetails.super.isEnabled();
+        return true;
+    }
 }
 
