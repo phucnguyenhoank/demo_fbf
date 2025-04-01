27642c33d311e927e3a06c51a0073cc4891f83be
HoangPhuc <phucnguyeho@gmail.com>
2025-04-01 14:56:30 +0700

    auth provider

--- src/main/java/com/example_fbf/demo_fbf/config/ApplicationConfig.java
+++ src/main/java/com/example_fbf/demo_fbf/config/ApplicationConfig.java
@@ -4,9 +4,13 @@ import com.example_fbf.demo_fbf.repository.FbfUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,5 +27,18 @@ public class ApplicationConfig {
                         .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
             }
         };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
